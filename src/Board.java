b536eeacfd1256f3c299d42b12f16db369331b4d
Thomas <tjm165@case.edu>
2019-03-16 15:36:56 -0400

    Tried to get User.addComment to work

--- src/Board.java
+++ src/Board.java
@@ -1,12 +1,28 @@
+import java.io.File;
+import java.util.ArrayList;
 
 public class Board {
+	public final static String BOARD_DIRECTORY = "board_directory/";
+	private ArrayList<Post> posts;
 
-  public Board(){
-    ArrayList<Post> posts = new ArrayList<>();
-  }
-  
-  public void addPost(Post post){
-    posts.add(Post)
-  }
-  
+	public Board() {
+		posts = new ArrayList<Post>();
+
+		File folder = new File(Board.BOARD_DIRECTORY);
+		File[] listOfFiles = folder.listFiles();
+
+		for (File file : listOfFiles)
+			if (file.isFile())
+				;// figure out what to do here
+
+	}
+
+	public void addPost(Post post) {
+		posts.add(post);
+	}
+
+	public Post getPost(int index) {
+		return posts.get(index);
+	}
+
 }
--- src/User.java
+++ src/User.java
@@ -7,32 +7,40 @@ import java.util.List;
 public class User {
 	private final static String BASE_NAME = "board_directory/File";
 	private String userId;
+	private Board board;
 
 	public User(String userId) {
 		this.userId = userId;
+		this.board = new Board();
 	}
 
 	public String getUserId() {
 		return userId;
 	}
 
-	public void addComment(Post post, String comment) throws FileNotFoundException {
-		
+	public void addComment(int postIndex, String comment) throws FileNotFoundException {
+		Post post = board.getPost(postIndex);
+		Content content = new Content("TEXT", comment);
+		Date dateCreated = Calendar.getInstance().getTime();
+
+		Comment commentObj = new Comment(content, dateCreated, userId);
+		post.addComment(commentObj);
 		post.saveToFile();
 	}
-	
+
 	private void createPost(String contentType, String content, boolean anon) throws FileNotFoundException {
 		Date dateCreated = Calendar.getInstance().getTime();
 		String pathname = User.BASE_NAME + dateCreated.getTime() + ".post";
-		
+
 		Content contentObj = new Content(contentType, content);
 		String userId = this.userId;
 		if (anon)
 			userId = null;
-		int interestLevel = 0;//might need to fix
+		int interestLevel = 0;// might need to fix
 		List<Comment> comments = new LinkedList<Comment>();
-		
+
 		Post post = new Post(pathname, contentObj, dateCreated, 0, userId, interestLevel, comments);
+		board.addPost(post);
 		post.saveToFile();
 	}
 
@@ -43,11 +51,12 @@ public class User {
 	public void createImagePost(String imgURL, boolean anon) throws FileNotFoundException {
 		this.createPost("IMAGE", imgURL, anon);
 	}
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
 		User user = new User("rocketman57");
-		user.createTextPost("hello world", false);
-	}
+		// user.createTextPost("hello world", false);
 
+		user.addComment(0, "I like that post");
+	}
 
 }
