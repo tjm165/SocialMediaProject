064bf7c53b7b59e82d085b9c6853093c8d950819
jaredjacknow <48361870+jaredjacknow@users.noreply.github.com>
2019-03-16 11:40:14 -0400

    Update Comment.java

--- src/Comment.java
+++ src/Comment.java
@@ -1,7 +1,20 @@
 
 public class Comment {
+	private Content content;
+	private Date dateCreated;
+	private String userId;
 
+	public Comment(Content content, Date datecreated, String userId){
+		this.content = content;
+		this.datecreated = datecreated;
+		this.userId = userId;
+	}
+	
 	public static Comment parse(String[] commentLines) {
+		Content content = new Content("text", commentLines[0]);
+		Date dateCreated = new Date();//NOT DONE ************
+		String userId = commentLines[2];
 		
+		return new Comment(content, dateCreated, userId);
 	}
 }
