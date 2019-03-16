8ed3705821e941e7bdf6098cca73cf23a3a77eac
jaredjacknow <48361870+jaredjacknow@users.noreply.github.com>
2019-03-16 11:44:57 -0400

    Update Content.java

--- src/Content.java
+++ src/Content.java
@@ -1,7 +1,23 @@
 
 public class Content {
+	private String type;
+	private String content;
 
 	public Content(String type, String content) {
-		
+		if(type.equals("TEXT") || type.equals("IMAGE")){
+			this.type = type;
+		}
+		else{
+			this.type = "TEXT";
+		}
+		this.content = content;
+	}
+	
+	public getType(){
+		return type;
+	}
+	
+	public getContent(){
+		return content;
 	}
 }
