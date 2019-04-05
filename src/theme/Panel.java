0ceee00c28a723be3ef2bac8a6128a6056e13dde
Thomas <tjm165@case.edu>
2019-04-05 16:33:43 -0400

    customized theme a bit more

--- src/theme/Panel.java
+++ src/theme/Panel.java
@@ -9,6 +9,6 @@ public class Panel extends JPanel{
 	public Panel(int rows, int cols) {
 		super(new GridLayout(rows, cols));
 		this.setBackground(Theme.COLOR_ACCENT1);
-		this.setBorder(Theme.BORDER);
+		//this.setBorder(Theme.BORDER);
 	}
 }
